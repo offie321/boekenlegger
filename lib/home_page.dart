1ac017c60823f6338022c31d719e47875b80b00e
Jelmer van Ofwegen <offie1@outlook.com>
2023-07-01 23:18:52 +0200

    Changed Homepage styling: Logout button and add new book button, removed top bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,17 +4,12 @@ import 'package:flutter/material.dart';
 import 'book.dart';
 import 'book_detail_page.dart';
 import 'add_book_page.dart';
-
+import 'login.dart';
 
 class HomePage extends StatelessWidget {
-
-
   Future<void> deleteBook(String bookId) async {
     try {
-      await FirebaseFirestore.instance
-          .collection('books')
-          .doc(bookId)
-          .delete();
+      await FirebaseFirestore.instance.collection('books').doc(bookId).delete();
 
       // Book deletion successful, handle UI updates or show a success message
     } catch (e) {
@@ -22,7 +17,6 @@ class HomePage extends StatelessWidget {
       print('Failed to delete book: $e');
     }
   }
-
 
   // Get the current user ID
   String getCurrentUserID() {
@@ -39,145 +33,169 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Book Library'),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('books')
-            .where('userId', isEqualTo: getCurrentUserID()) // Filter books by the authenticated user ID
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final List<Book> books = snapshot.data!.docs
-                .map((doc) => Book.fromMap(doc.data() as Map<String, dynamic>))
-                .toList()
-                .cast<Book>();
-
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 25, top: 50, bottom: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        'Hi Robbert',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey,
-                        ),
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 25, top: 50, bottom: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Hi Robbert',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
                       ),
-                      Text(
-                        'Book Library',
-                        style: TextStyle(
-                          fontSize: 26,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    ),
+                    Text(
+                      'Book Library',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: books.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final book = books[index];
-                      final bookDocument = snapshot.data!.docs[index];
-                      final bookId = bookDocument.id; // Retrieve the document ID
-                      final double percentage = (book.currentPageCount / book.pageCount) * 100;
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  BookDetailPage(book: book,bookId: bookId,deleteBook: deleteBook),
+              ),
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('books')
+                      .where('userId', isEqualTo: getCurrentUserID()) // Filter books by the authenticated user ID
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final List<Book> books = snapshot.data!.docs
+                          .map((doc) => Book.fromMap(doc.data() as Map<String, dynamic>))
+                          .toList()
+                          .cast<Book>();
+
+                      return ListView.builder(
+                        itemCount: books.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final book = books[index];
+                          final bookDocument = snapshot.data!.docs[index];
+                          final bookId = bookDocument.id; // Retrieve the document ID
+                          final double percentage = (book.currentPageCount / book.pageCount) * 100;
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => BookDetailPage(
+                                    book: book,
+                                    bookId: bookId,
+                                    deleteBook: deleteBook,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Card(
+                              margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              elevation: 3,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white,
+                                ),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      width: 80,
+                                      height: 120,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(10),
+                                          bottomLeft: Radius.circular(10),
+                                        ),
+                                        image: DecorationImage(
+                                          image: NetworkImage(books[index].imageUrl),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(12.0),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              books[index].title,
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                            SizedBox(height: 4),
+                                            Text(
+                                              '${books[index].pageCount} pages',
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.grey[600],
+                                              ),
+                                            ),
+                                            SizedBox(height: 4),
+                                            Text(
+                                              'Progress: ${percentage.toStringAsFixed(2)}%',
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.grey[600],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
                           );
                         },
-                        child: Card(
-                          margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          elevation: 3,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white,
-                            ),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: 80,
-                                  height: 120,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(10),
-                                      bottomLeft: Radius.circular(10),
-                                    ),
-                                    image: DecorationImage(
-                                      image: NetworkImage(books[index].imageUrl),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(12.0),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                      CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                      MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          books[index].title,
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 16,
-                                          ),
-                                        ),
-                                        SizedBox(height: 4),
-                                        Text(
-                                          '${books[index].pageCount} pages',
-                                          style: TextStyle(
-                                            fontSize: 14,
-                                            color: Colors.grey[600],
-                                          ),
-                                        ),
-                                        SizedBox(height: 4),
-                                        Text(
-                                          'Progress: ${percentage.toStringAsFixed(2)}%',
-                                          style: TextStyle(
-                                            fontSize: 14,
-                                            color: Colors.grey[600],
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
                       );
-                    },
-                  ),
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  },
                 ),
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
+              ),
+            ],
+          ),
+          Positioned(
+            top: 50,
+            right: 25,
+            child: Container(
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                color: Colors.orangeAccent,
+              ),
+              child: IconButton(
+                icon: Icon(Icons.logout),
+                color: Colors.white,
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -187,8 +205,12 @@ class HomePage extends StatelessWidget {
           );
         },
         child: Icon(Icons.add),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        backgroundColor: Colors.orangeAccent, // Set the background color to yellow
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
-
